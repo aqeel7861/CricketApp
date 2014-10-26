ff4454fd990bb1e1794d6bcca3185c1deca1a6d3
Aqeel Rafiq <aqeelrafiq@aqeels-mbp.home>
2014-10-26 11:51:36 +0000

    Initial Commit

---
+++ CricketApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CricketApp
+//
+//  Created by Aqeel Rafiq on 26/10/2014.
+//  Copyright (c) 2014 Aqeel Rafiq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
