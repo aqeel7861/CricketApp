66403a5388c44d9e99bcfe8aa804498f094003fc
Aqeel <aqeelrafiq@hotmail.co.uk>
2015-04-07 12:05:45 +0100

    Unit tests

--- CricketAppTests/CricketAppTests.m
+++ CricketAppTests/CricketAppTests.m
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "InternetController.h"
 
 @interface CricketAppTests : XCTestCase
 
@@ -25,9 +26,66 @@
     [super tearDown];
 }
 
+
+float angleBetweenLines(const CGPoint line1Start, const CGPoint line1End, const CGPoint line2Start, const CGPoint line2End)
+{
+    //each line has a start point and end get angle for it
+    const float a =line1End.x -line1Start.x;
+    const float b =line1End.y -line1Start.y;
+    const float c= line2End.x -line2Start.x;
+    const float d= line2End.y- line2Start.y;
+    
+    //calculate angle in radians
+    const float rads = acosf( ( ( a*c) + (b*d))/ (( sqrt( a*a + b*b)) * (sqrtf(c*c + d*d))));
+    
+    return ( ( 180.0f* rads) / M_PI);
+    
+    //    const float rads = acosf( ( ( a*c ) + ( b*d ) ) / ( ( sqrtf( a*a + b*b ) ) * ( sqrtf( c*c + d*d ) ) ) );
+}
+- (void)testAngles
+{
+    CGPoint l1s = CGPointMake(0.0f, 0.0f);
+    CGPoint l1e = CGPointMake(0.0f, 1.0f);
+    CGPoint l2s = CGPointMake(0.0f, 0.0f);
+    CGPoint l2e = CGPointMake(1.0f, 1.0f);
+    
+    const float angle = angleBetweenLines(l1s, l1e, l2s, l2e);
+    
+    if( angle > 89.0f && angle < 91.0f )
+    {
+        XCTAssert(YES, @"Pass");
+    }
+    else
+    {
+        XCTAssert(NO, @"Fail %f", angle);
+    }
+}
+
 - (void)testExample {
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"asynchronous request"];
+    
     // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+    InternetController *internetController = [[InternetController alloc] init];
+    [internetController getVideosList:^(NSMutableData *data, NSString *filename){
+        if( data != nil )
+        {
+            NSString *stringData = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            if( stringData != NULL )
+            {
+                XCTAssert(YES, @"Pass");
+            }
+            else
+            {
+                XCTAssert(NO, @"Pass");
+            }
+            
+            [expectation fulfill];
+        }
+    }];
+    
+    [self waitForExpectationsWithTimeout:10.0 handler:nil];
+    
 }
 
 - (void)testPerformanceExample {
